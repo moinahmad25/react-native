5674391f67f6df5f4f5fec3c31e3806be63a0182
shivamch023 <shivamchauhan2499@gmail.com>
2024-09-13 01:01:40 +0530

    added new pages and some changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,19 +2,28 @@ import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-
   // defining custom fonts
   useFonts({
-    'outfit-thin': require('../assets/fonts/Outfit-Thin.ttf'),
-    'outfit': require('../assets/fonts/Outfit-Regular.ttf'),
-    'outfit-medium': require('../assets/fonts/Outfit-Medium.ttf'),
-    'outfit-bold': require('../assets/fonts/Outfit-Bold.ttf'),
-  })
+    "outfit-thin": require("../assets/fonts/Outfit-Thin.ttf"),
+    outfit: require("../assets/fonts/Outfit-Regular.ttf"),
+    "outfit-medium": require("../assets/fonts/Outfit-Medium.ttf"),
+    "outfit-bold": require("../assets/fonts/Outfit-Bold.ttf"),
+  });
 
-  
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="auth/sign-in/index"
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 }
--- components/Login.jsx
+++ components/Login.jsx
@@ -1,13 +1,23 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { Colors } from '@/constants/Colors'
+import { useRouter } from 'expo-router'
 
 const Login = () => {
+
+  //defining router for traveling from one page to another with a button or any small interaction. It is different from traversing from one page to another pages
+
+  const router = useRouter()
+
   return (
     <View>
       <Image source={require('./../assets/images/start_poster.png')} style={styles.loginPoster} />
       <View style={styles.container}>
         <Text style={styles.loginText}>AI Travel Planner</Text>
+        <Text style={styles.subText}>Discover your next adventure effortlessly. Personalized itineraries at your fingertips. Travel smarter with AI-Driven insights.</Text>
+        <TouchableOpacity style={styles.button} onPress={() => router.push('auth/sign-in')}>
+          <Text style={styles.buttonText}>Sign in with Google</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
@@ -16,22 +26,46 @@ const Login = () => {
 export default Login
 
 const styles = StyleSheet.create({
-    loginPoster:{
-        width: '100%',
-        height: 450
-    },
-    container: {
-        width: '100%',
-        marginTop: -20,
-        backgroundColor: Colors.WHITE,
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        height: '100%',
-        padding: 20
-    },
-    loginText: {
-        fontSize: 28,
-        fontFamily: 'outfit-bold',
-        textAlign: 'center'
-    }
+  loginPoster: {
+    width: '100%',
+    height: 530
+  },
+  container: {
+    width: '100%',
+    marginTop: -20,
+    backgroundColor: Colors.WHITE,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    height: '100%',
+    padding: 20
+  },
+  loginText: {
+    fontSize: 28,
+    fontFamily: 'outfit-bold',
+    textAlign: 'center',
+    marginTop: 20
+  },
+  subText: {
+    fontFamily: 'outfit',
+    fontSize: 17,
+    textAlign: 'center',
+    color: Colors.GRAY,
+    marginTop: 12
+  },
+  button: {
+    backgroundColor: Colors.BLACK,
+    borderRadius: 20,
+    padding: 16,
+    marginTop: '18%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: Colors.WHITE,
+    textAlign: 'center',
+    fontSize: 18,
+    fontFamily: 'outfit'
+
+  },
 })
